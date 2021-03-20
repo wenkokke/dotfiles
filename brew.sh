9fab0b2535de28543c5239812f26bc8de1706fee
Wen Kokke <wenkokke@users.noreply.github.com>
2021-03-20 11:55:51 +0000

    Custom settings.

--- brew.sh
+++ brew.sh
@@ -32,70 +32,47 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install aspell
+brew install cloc
+brew install dark-mode
+brew install epubcheck
+brew install ffmpeg
+brew install fswatch
 brew install git
 brew install git-lfs
+brew install gnupg
 brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+brew install hyperfine
+brew install imagemagick
+brew install pandoc
 brew install rename
-brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+brew install wget
+brew install youtube-dl
+brew install yq
+
+# Install programming languages.
+brew install gcc
+brew install haskell-stack
+brew install node
+brew install opam
+brew install python
+brew install ruby
+brew install texlab
+brew install z3
+
+# Install rustup and ghcup.
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 # Remove outdated versions from the cellar.
 brew cleanup
